9aad691432546d6fa1689665ddb242fc1caec427
aralzaim <aralzaim7@gmail.com>
2015-08-01 13:51:42 +0100

    Room Details being implemented as a tab

--- src/main/java/com/example/android/tabbedroombookingtimetabledisplay/RoomDetailsActivity.java
+++ src/main/java/com/example/android/tabbedroombookingtimetabledisplay/RoomDetailsActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.tabbedroombookingtimetabledisplay;
 
-import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,16 +10,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
-import com.squareup.picasso.Picasso;
+import com.example.android.tabbedroombookingtimetabledisplay.helpers.Converters;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by aralzaim on 31/07/15.
@@ -30,6 +39,9 @@ public class RoomDetailsActivity extends Fragment {
 
     ImageView outside1;
     ImageView outside2;
+    Converters converters= new Converters();
+    Spinner roomSpinner;
+
 
 
     @Override
@@ -37,6 +49,7 @@ public class RoomDetailsActivity extends Fragment {
                              Bundle savedInstanceState) {
         RelativeLayout rootView = (RelativeLayout) (inflater.inflate(R.layout.activity_details, container, false));
 
+        roomSpinner = (Spinner) rootView.findViewById(R.id.spinner2);
         outside1=(ImageView) rootView.findViewById(R.id.imageView);
         outside2=(ImageView) rootView.findViewById(R.id.imageView2);
 
@@ -45,7 +58,8 @@ public class RoomDetailsActivity extends Fragment {
 
         //Picasso.with(getActivity()).load("https://zeno.computing.dundee.ac.uk/2014-msc/aralzaim/images/GOPR3414.JPG").into(outside2);
       //  outside2.setImageBitmap(getBitmapFromURL("https://zeno.computing.dundee.ac.uk/2014-msc/aralzaim/images/GOPR3414.PNG"));
-
+        GetRoomsBooking getRooms= new GetRoomsBooking();
+            getRooms.execute();
 
         return rootView;
     }
@@ -66,6 +80,99 @@ public class RoomDetailsActivity extends Fragment {
             Log.e("Exception",e.getMessage());
             return null;
         }
+    }
+
+    private class GetRoomsBooking extends AsyncTask <String, Void, Boolean>{
+
+        ProgressDialog fetchingDialog;
+        HttpClient httpClient=null;
+        HttpGet httpGet=null;
+        HttpResponse httpResponse=null;
+        ArrayList<String> roomNames = new ArrayList<>();
+
+        @Override
+        protected void onPreExecute(){
+
+            fetchingDialog = new ProgressDialog(getActivity());
+            fetchingDialog.setMessage("Fetching Rooms..");
+            fetchingDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            httpClient= new DefaultHttpClient();
+            httpGet = new HttpGet("https://zeno.computing.dundee.ac.uk/2014-msc/aralzaim/getRooms.php");
+
+            try {
+                httpResponse= httpClient.execute(httpGet);
+
+
+                jsonRoomParse(httpResponse);
+
+            } catch (IOException e) {
+
+                e.toString();
+            }
+            return true;
+
+        }
+
+        public void jsonRoomParse(HttpResponse httpResponse) {
+            String jsonResult;
+
+            if(roomNames.size()<=0) {
+                try {
+                    roomNames.add("Select a room...");
+                    jsonResult = converters.inputStreamToString(httpResponse.getEntity().getContent()).toString();
+
+                    JSONObject jsonObj = new JSONObject(jsonResult);
+
+                    if (jsonObj != null) {
+                        JSONArray rooms = jsonObj.getJSONArray("rooms");
+
+                        for (int i = 0; i < rooms.length(); i++) {
+                            JSONObject catObj = (JSONObject) rooms.get(i);
+
+                            roomNames.add(catObj.getString("room_name"));
+                        }
+                    }
+
+
+                } catch (IllegalStateException | IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        @Override
+        protected void onPostExecute(Boolean result){
+            createSpinner(roomNames);
+            fetchingDialog.dismiss();
+
+        }
+
+        public void createSpinner(ArrayList<String> roomNames) {
+
+            //MyCustomAdapter adapter = new MyCustomAdapter(roomNames, this);
+
+            //handle listview and assign adapter
+            //ListView lView = (ListView)findViewById(R.id.my_listview);
+            //lView.setAdapter(adapter);
+
+            ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.spinner_item, roomNames);
+
+            spinnerAdapter
+                    .setDropDownViewResource(R.layout.list_item);
+
+            roomSpinner.setAdapter(spinnerAdapter);
+        }
+
     }
 
 
